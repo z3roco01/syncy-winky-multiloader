c54d6e68061a92a7932c8c22852766ade12de007
z3roco01 <zerocool@e.email>
2026-09-15 09:06:45 -0500

    remove a few poops

--- build.forge.gradle.kts
+++ build.forge.gradle.kts
@@ -117,8 +117,6 @@ stonecutter {
 }
 
 val requiredJava = when {
-    sc.current.parsed >= "26.1" -> JavaVersion.VERSION_25
-    sc.current.parsed >= "1.20.5" -> JavaVersion.VERSION_21
     sc.current.parsed >= "1.18" -> JavaVersion.VERSION_17
     sc.current.parsed >= "1.17" -> JavaVersion.VERSION_16
     else -> JavaVersion.VERSION_1_8
